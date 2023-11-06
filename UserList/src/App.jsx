cca1c418a5ba4456a98588196738e4b0b288679a
EnesHHayrula <eneshayrula@gmail.com>
2023-11-06 16:49:10 +0200

    modified:   UserList/components/UserList.jsx 	modified:   UserList/components/UserListItem.jsx 	modified:   UserList/components/UserListTable.jsx 	modified:   UserList/src/App.jsx 	modified:   UserList/src/main.jsx 	modified:   UserList/src/services/userService.js 	modified:   UserList/src/utils/dataUtils.js

--- UserList/src/App.jsx
+++ UserList/src/App.jsx
@@ -1,19 +1,21 @@
-import Footer from "../components/Footer";
-import UserList from "../components/UserList";
-import Header from "../components/Header";
-import "./styles.css";
+import Footer from './components/Footer';
+import Header from './components/Header';
+import UserList from './components/UserList';
+import './styles.css';
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <main className="main">
-      <UserList />
 
-      </main>
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+
+            <main className="main">
+                <UserList />
+            </main>
+
+            <Footer />
+        </div>
+    );
 }
 
-export default App;
+export default App
