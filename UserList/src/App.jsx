9ec804510f19c9e81ea5aff6c62210e82a8aefb6
EnesHHayrula <eneshayrula@gmail.com>
2023-11-06 16:51:55 +0200

    modified:   UserList/index.html 	modified:   UserList/indexTemplate.html 	modified:   UserList/src/App.jsx

--- UserList/src/App.jsx
+++ UserList/src/App.jsx
@@ -1,6 +1,6 @@
-import Footer from './components/Footer';
-import Header from './components/Header';
-import UserList from './components/UserList';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import UserList from '../components/UserList';
 import './styles.css';
 
 function App() {
